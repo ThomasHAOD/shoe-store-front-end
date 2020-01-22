d233191ca2601c06739272a8ab7fce15dfb49f6e
TamHOD <thomashodonnell@gmail.com>
2020-01-22 12:53:41 +0000

    add mock account details page

--- src/containers/Account/Account.js
+++ src/containers/Account/Account.js
@@ -1,8 +1,34 @@
 import React, { Component } from "react";
+import classes from "./Account.module.css";
 
 export class Account extends Component {
+  state = {
+    user: {
+      firstName: "Tam",
+      lastName: "OD",
+      houseNumber: "1",
+      street: "street street",
+      town: "Edinburgh",
+      postCode: "EH1 1AA"
+    }
+  };
   render() {
-    return <div>Account</div>;
+    return (
+      <div className={classes.AccountDetails}>
+        <h1>Your Account</h1>
+        <h3>Your Details</h3>
+        <ul>
+          <li>
+            Name: {this.state.user.firstName} {this.state.user.lastName}
+          </li>
+          <li>
+            Address: {this.state.user.houseNumber} {this.state.user.street}
+          </li>
+          <li>{this.state.user.town}</li>
+          <li>{this.state.user.postCode}</li>
+        </ul>
+      </div>
+    );
   }
 }
 
