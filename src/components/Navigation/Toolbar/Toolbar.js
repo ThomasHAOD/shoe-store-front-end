4d8a61d5cc3e1c8e1fd1c814b2714b10157034f8
TamHOD <thomashodonnell@gmail.com>
2020-01-22 19:55:53 +0000

    style sign in and sign up links

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -7,14 +7,14 @@ import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 const toolbar = props => {
   return (
     <header className={classes.Toolbar}>
+      <ul className={classes.Sign}>
+        <li onClick={props.signIn}>Sign In</li>
+        <li onClick={props.signUp}>Sign Up</li>
+      </ul>
       <DrawerToggle clicked={props.drawerToggleClicked} />
       <nav className={classes.DesktopOnly}>
         <NavigationItems />
       </nav>
-      <ul>
-        <li onClick={props.signIn}>Sign In</li>
-        <li onClick={props.signUp}>Sign Up</li>
-      </ul>
     </header>
   );
 };
