6c058dafcc4a0bd24485b199d0a75a5660413895
TamHOD <thomashodonnell@gmail.com>
2020-01-25 08:13:12 +0000

    logo showing in middle of toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -5,24 +5,30 @@ import NavigationItems from "../NavigationItems/NavigationItems";
 import classes from "./Toolbar.module.css";
 import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 import BasketSummary from "../../../containers/Basket/BasketWidget/BasketWidget";
+import Logo from "../../../assets/Golden-shoe-logo.png";
+import Button from "../../UI/Button/Button";
 
 const toolbar = props => {
   let info = (
-    <ul className={classes.Sign}>
-      <li onClick={props.signUp}>Sign Up</li>
-    </ul>
+    <Button btnType={"Success"} clicked={props.signUp}>
+      Sign Up
+    </Button>
   );
 
   if (props.signedIn) {
-    info = <BasketSummary showBasket={props.showBasket} />;
+    info = (
+      <BasketSummary className={classes.Sign} showBasket={props.showBasket} />
+    );
   }
   return (
     <header className={classes.Toolbar}>
-      <DrawerToggle clicked={props.drawerToggleClicked} />
+      {info}
+
+      <img className={classes.Logo} src={Logo} alt="golden-shoe.logo" />
       <nav className={classes.DesktopOnly}>
         <NavigationItems />
       </nav>
-      {info}
+      <DrawerToggle clicked={props.drawerToggleClicked} />
     </header>
   );
 };
